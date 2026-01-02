f37869c4638a1948f19556f93c555f2fa5d07ffc
Aachal <aachal.agarwal@openxcell.com>
2026-01-02 17:48:41 +0530

    Update SQL files made database again

--- sql/E-wallet DB.session.sql
+++ sql/E-wallet DB.session.sql
@@ -17,4 +17,42 @@ CREATE TABLE transactions(
 );
 
 
+INSERT INTO accounts (acc_name, balance, email)
+VALUES
+    ('Achal', 1200, 'achal@gmailcom'),
+    ('Neha', 850, 'Neha@example.com'),
+    ('Priya', 3000, 'Priya@example.com');
+
+select * from accounts
+
+
+
+INSERT INTO transactions(
+    acc_id,amount
+)VALUES(1,500);
+
+select * from accounts
+
+select * from transactions
+
+
+//changing the table structure 
+ drop table transactions;
+
+CREATE TABLE transactions (
+    tx_id BIGSERIAL PRIMARY KEY,
+    from_acc_id INTEGER NOT NULL REFERENCES accounts(acc_id),
+    to_acc_id INTEGER NOT NULL REFERENCES accounts(acc_id),
+    amount NUMERIC NOT NULL CHECK (amount > 0), 
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO transactions(from_acc_id,to_acc_id,amount)
+VALUES(1,2,500)
+
+
+
+
+
 
